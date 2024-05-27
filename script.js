642849dd931705aad47fd8d7be5c439277d8680c
Carlos Rafael <carlosrafael360@gmail.com>
2024-05-27 16:11:07 -0300

    script completo

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+const list = document.querySelector("ul");
+
+function  renderElements(array) {
+  for(let i = 0; i < array.length; i++){
+    const title = array[i].title;
+    const type = array[i].type;
+    createTaskItem(title, type);
+    
+
+  }
+}
+
+function  createTaskItem(title ,type){
+
+    const listItem = document.createElement("li");
+    const listContainer = document.createElement("div");
+    const listSpan = document.createElement("span");
+    const listTitle = document.createElement("p");
+    const listButton = document.createElement("button");
+
+    listTitle.innerText  = title; 
+
+    if(type === "Urgente"){
+       listSpan.classList.add("span-urgent");
+    }else if(type === "Importante"){
+       listSpan.classList.add("span-important");
+    }else{
+       listSpan.classList.add("span-normal");
+    }
+
+    listItem.classList.add("task__item");
+    listContainer.classList.add("task-info__container");
+    listSpan.classList.add("task-type");
+    listButton.classList.add("task__button--remove-task");
+
+    listItem.appendChild(listContainer);
+    listItem.appendChild(listButton);
+    listContainer.appendChild(listSpan);
+    listContainer.appendChild(listTitle);
+    list.appendChild(listItem);  
+    console.log(listItem);
+  }  
+
+renderElements(tasks)
